3ca8fc14eccf2cc31da080f18c1d8a6a6c431685
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-08-08 09:38:27 -0400

    Updated import: testing all data

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,27 +10,26 @@ import { MongooseModule } from '@nestjs/mongoose';
 // import { TestingModule } from './testing/testing-module';
 // import { PostsModule } from './posts/posts.module';
 // import { BlogsModule } from './blogs/blogs.module';
-import { UsersRepository } from './features/users/users.repo';
-import { UsersService } from './features/users/users.service';
+import { UsersRepository } from './features/users/infrastructure/users.repo';
+import { UsersService } from './features/users/application/users.service';
 import { appSettings } from './settings/app.setting';
-import { User, UsersSchema } from './features/users/users.schema';
-import { UsersController } from './features/users/users.controller';
+import { User, UsersEntity } from './features/users/domain/users.entity';
+import { UsersController } from './features/users/api/users.controller';
 import { LoggerMiddleware } from './infrastructure/middlewares/logger.middleware';
 import {UsersModule} from "./features/users/users.module";
+import {TestingModule} from "./features/testing/testing-module";
+
 
 const usersProviders: Provider[] = [UsersRepository, UsersService];
 
 @Module({
   imports: [
-    //MongooseModule.forRoot(process.env.MONGO_CONNECTION_URI as string),
-    // DatabaseModule,
-    //UsersModule,
-    // TestingModule,
     // PostsModule,
     // BlogsModule,
     MongooseModule.forRoot(appSettings.api.MONGO_CONNECTION_URI),
-    MongooseModule.forFeature([{ name: User.name, schema: UsersSchema }]),
-
+    MongooseModule.forFeature([{ name: User.name, schema: UsersEntity }]),
+    UsersModule,
+    TestingModule,
   ],
   providers: [...usersProviders],
   controllers: [UsersController],
