ddaad1dd8a4158eff97801c92ada728dc728e9cf
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-08-22 11:51:46 -0300

    Created routes for auth user endpoint

--- src/features/auth/api/auth.controller.ts
+++ src/features/auth/api/auth.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
+import {UsersService} from "../../users/application/users.service";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private usersService: UsersService,
+    ) {}
+
+    // @Post('/login')
+    // async loginUser(
+    //     @Body()
+    // loginUserDto: loginUserDto) {
+    //
+    // }
+    //
+    // @Post('/password-recovery')
+    // async recoveryPassword(
+    //     @Body()
+    //     loginUserDto: loginUserDto) {
+    //
+    // }
+    //
+    // @Post('/new-password')
+    // async createNewPassword(
+    //     @Body()
+    //         loginUserDto: loginUserDto) {
+    //
+    // }
+    //
+    // @Post('/registration-confirmation')
+    // async confirmRegistration(
+    //     @Body()
+    //         loginUserDto: loginUserDto) {
+    //
+    // }
+    //
+    // @Post('/registration')
+    // async registerUser(
+    //     @Body()
+    //         loginUserDto: loginUserDto) {
+    //
+    // }
+    //
+    // @Post('/registration-email-sending')
+    // async registerEmailSending(
+    //     @Body()
+    //         loginUserDto: loginUserDto) {
+    //
+    // }
+
+    @Get('/me')
+    async getUser(
+        @Body()
+            loginUserDto: loginUserDto) {
+
+    }
+}
