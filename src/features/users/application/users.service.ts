a3c651eb1364386e370a09d9dcb36c65fdeabaf3
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-05 17:58:13 -0300

    Modified user service: get all users

--- src/features/users/application/users.service.ts
+++ src/features/users/application/users.service.ts
@@ -1,105 +1,110 @@
 import {ConflictException, Injectable} from '@nestjs/common';
-import { User } from '../domain/users.entity';
-import * as bcrypt from 'bcrypt';
+import {User} from '../domain/users.entity';
 
-import { UsersRepository } from '../infrastructure/users.repository';
+import {UsersRepository} from '../infrastructure/users.repository';
 import {UserOutputModelMapper} from "../api/models/output/user.output.model";
+import {UsersQueryRepository} from "../infrastructure/users.query-repository";
 
 
 @Injectable()
 export class UsersService {
-  constructor(private usersRepository: UsersRepository) {}
-
-  async create(
-    email: string,
-    login: string,
-    password: string,
-  ): Promise<{ id: string; login: string; email: string; createdAt: Date }> {
-     const existingUser = await this.usersRepository.findOneByEmail(email);
-    if (existingUser) {
-      throw new ConflictException(`Blog with name "${name}" already exists`);
+    constructor(
+        private usersRepository: UsersRepository,
+        private usersQueryRepository: UsersQueryRepository,
+    ) {
     }
-    // email send message
-    // this.emailAdapter.send(message);
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    async create(
+        email: string,
+        login: string,
+        password: string,
+    ): Promise<{ id: string; login: string; email: string; createdAt: Date }> {
 
-    const user = User.create(login, email, hashedPassword);
-    const createdUser = await this.usersRepository.create(user);
+        const existingUser = await this.usersQueryRepository.findOneByEmail(email);
 
-    return {
-      id: createdUser.id.toString(),
-      login: createdUser.login,
-      email: createdUser.email,
-      createdAt: createdUser.createdAt,
-    };
-  }
+        if (existingUser) {
+            throw new ConflictException(`User with this email already exists`);
+        }
 
-  // async getById(userId: string): Promise<UserOutputModel | null> {
-  //   return this.usersRepository.getById(userId);
-  // }
+        const user = await User.create(login, email, password);
+        const createdUser = await this.usersRepository.create(user);
 
-  async validateUser(email: string, password: string): Promise<User | null> {
-    const user = await this.usersRepository.findOneByEmail(email);
-    if (user && (await bcrypt.compare(password, user.password))) {
-      return user;
+        return {
+            id: createdUser.id.toString(),
+            login: createdUser.accountData.login,
+            email: createdUser.accountData.email,
+            createdAt: createdUser.accountData.createdAt,
+        };
     }
-    return null;
-  }
 
-  async findAll(): Promise<User[]> {
-    return this.usersRepository.findAll();
-  }
+    // async getById(userId: string): Promise<UserOutputModel | null> {
+    //   return this.usersRepository.getById(userId);
+    // }
 
-  async findOneByEmail(email: string): Promise<User | null> {
-    const user = await this.usersRepository.findOneByEmail(email);
-    if (user)  {
-      return user;
+
+    async findAll(): Promise<User[]> {
+        return this.usersRepository.findAll();
     }
-    return null;
-  }
 
-  async findOneByLogin(login: string): Promise<User | null> {
-    const user = await this.usersRepository.findOneByLogin(login);
-    if (user)  {
-      return user;
+    async findOneByEmail(email: string): Promise<User | null> {
+        const user = await this.usersQueryRepository.findOneByEmail(email);
+        if (user) {
+            return user;
+        }
+        return null;
     }
-    return null;
-  }
 
-  async deleteUserById(id: string): Promise<boolean> {
-    return await this.usersRepository.deleteById(id);
-  }
+    async findOneByLogin(login: string): Promise<any | null> {
+        const user = await this.usersRepository.findOneByLogin(login);
+        if (user) {
+            return user;
+        }
+        return null;
+    }
 
-  async findAllPaginated(
-    sort: string,
-    direction: 'asc' | 'desc',
-    page: number,
-    pageSize: number,
-    searchLogin: string,
-    searchEmail: string,
-  ) {
-    const { users, totalCount } = await this.usersRepository.findAllPaginated(
-      sort,
-      direction,
-      page,
-      pageSize,
-      searchLogin,
-      searchEmail,
-    );
+    async deleteUserById(id: string): Promise<boolean> {
+        return await this.usersRepository.deleteById(id);
+    }
 
-    const mappedUsers = users.map(UserOutputModelMapper);
+    async findAllPaginated(
+        sort: string,
+        direction: 'asc' | 'desc',
+        page: number,
+        pageSize: number,
+        searchLogin: string,
+        searchEmail: string,
+    ) {
+        const {users, totalCount} = await this.usersQueryRepository.findAllPaginated(
+            sort,
+            direction,
+            page,
+            pageSize,
+            searchLogin,
+            searchEmail,
+        );
 
-    // const mappedUsers = users.map((user) => ({
-    //   id: user._id.toString(),
-    //   login: user.login,
-    //   email: user.email,
-    //   createdAt: user.createdAt,
-    // }));
+        const mappedUsers = users.map(UserOutputModelMapper);
 
-    return {
-      users: mappedUsers,
-      totalCount,
-    };
-  }
+        return {
+            users: mappedUsers,
+            totalCount,
+        };
+    }
+
+    async checkCredentials(loginOrEmail: string, password: string) {
+
+        const user = await this.usersQueryRepository.findOneByLoginOrEmail(loginOrEmail);
+
+        if (!user) {
+            return null;
+        }
+
+        //const isMatch = await bcrypt.compare(password, user.password);
+
+        // if (isMatch) {
+        //     return user;
+        // }
+
+        return null;
+    }
 }
