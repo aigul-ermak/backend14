6d8572ff7d1bc92ec7a748b22f209d1b4974b916
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-06 17:04:26 -0300

    Auth functionality: formatted code, changed login user func

--- src/features/auth/api/auth.controller.ts
+++ src/features/auth/api/auth.controller.ts
@@ -1,9 +1,25 @@
-import {Body, Controller, Get, HttpCode, HttpStatus, Post, Req, UseGuards} from '@nestjs/common';
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Post,
+    Req,
+    UnauthorizedException,
+    UseGuards,
+    ValidationPipe
+} from '@nestjs/common';
 import {UsersService} from "../../users/application/users.service";
 import {UsersQueryRepository} from "../../users/infrastructure/users.query-repository";
 import {AuthService} from "../application/auth.service";
 import {AuthGuard} from "@nestjs/passport";
 import {LocalAuthGuard} from "../local-auth.guard";
+import {BasicAuthGuard} from "../basic-auth.guard";
+import {UserLoginDto} from "./models/input/login-user.input.dto";
+import {CreateUserDto} from "../../users/api/models/input/create-user.input.dto";
+import {UserOutputModel} from "../../users/api/models/output/user.output.model";
 
 
 @Controller('auth')
@@ -11,34 +27,36 @@ export class AuthController {
     userService: UsersService;
 
     constructor(
-        private authService: AuthService,
-        private readonly usersQueryRepository: UsersQueryRepository,
+        private authService: AuthService
     ) {
         this.authService = authService;
     }
 
     //@HttpCode(HttpStatus.OK)
-    @UseGuards(LocalAuthGuard)
+
+    // @UseGuards(LocalAuthGuard)
     @Post('/login')
-    async login(@Req() req) {
-        return this.authService.login(req.user);
+    @HttpCode(200)
+    async login(@Body() loginDto: UserLoginDto) {
+
+        const user = await this.authService.validateUser(loginDto.loginOrEmail, loginDto.password);
+
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        return this.authService.login(user);
     }
 
-
-// @UseGuards(AuthGuard)
-// @Get('profile')
-// getProfile(@Request() req) {
-//     return req.user;
-// }
-
-//
+    //
 // @Post('/password-recovery')
 // async recoveryPassword(
 //     @Body()
 //     loginUserDto: loginUserDto) {
 //
 // }
-//
+
+    //
 // @Post('/new-password')
 // async createNewPassword(
 //     @Body()
@@ -46,39 +64,43 @@ export class AuthController {
 //
 // }
 //
-// @Post('/registration-confirmation')
-// async confirmRegistration(
-//     @Body()
-//         loginUserDto: loginUserDto) {
-//
-// }
-//
-// @Post('/registration')
-// async registerUser(
-//     @Body()
-//         loginUserDto: loginUserDto) {
-//
-// }
-//
-// @Post('/registration-email-sending')
-// async registerEmailSending(
-//     @Body()
-//         loginUserDto: loginUserDto) {
-//
-// }
 
-// @Get('/me')
-// async getUser(
-//     @Req() req: Request
-// ): Promise<UserOutputModel> {
-//     const userId = req.user!.id
-//
-//     const user = await this.usersQueryRepository.getById(userId);
-//
-//     if (!user) {
-//         throw new Error('User not found');
-//     }
-//
-//     return user;
-// }
+    // @Post('/registration-confirmation')
+    // @HttpCode(204)
+    // async confirmRegistration(code: string) {
+    //     const result = await this.authService.confirmEmail(code)
+    //
+    //     if (!result) {
+    //         throw new BadRequestException()
+    //     }
+    // }
+
+    @Post('/registration')
+    @HttpCode(204)
+    async registration(
+        @Body() createUserDto: CreateUserDto) {
+
+        const result = await this.authService.createUser(
+            createUserDto
+        );
+
+    }
+
+    // @Post('/registration-email-sending')
+    // @HttpCode(204)
+    // async registerEmailSending() {
+    //
+    //     const result: boolean = await this.authService.confirmEmail(code);
+    //
+    //     if (!result) {
+    //         throw new BadRequestException()
+    //     }
+    //
+    // }
+
+    // @Get('/me')
+    // async getUser(
+    //     @Req() req: Request
+    // ) {
+    // }
 }
--- src/features/auth/application/auth.service.ts
+++ src/features/auth/application/auth.service.ts
@@ -1,11 +1,17 @@
-import {BadRequestException, ConflictException, Injectable, UnauthorizedException} from '@nestjs/common';
+import {BadRequestException, Injectable, UnauthorizedException} from '@nestjs/common';
 import {UsersService} from "../../users/application/users.service";
 import {JwtService} from "@nestjs/jwt";
-import {User, UserDocument} from "../../users/domain/users.entity";
+import {User} from "../../users/domain/users.entity";
 import {UsersQueryRepository} from "../../users/infrastructure/users.query-repository";
 import {UsersRepository} from "../../users/infrastructure/users.repository";
-import * as bcrypt from "bcrypt";
 import {EmailService} from "../../email/email.service";
+import { v4 as uuidv4 } from 'uuid';
+import {CreateUserDto} from "../../users/api/models/input/create-user.input.dto";
+import {UserDBType} from "../../users/types/user.types";
+import bcrypt from "bcrypt";
+import * as dateFns from "date-fns";
+
+
 
 
 export type AccessToken = string;
@@ -23,24 +29,17 @@ export class AuthService {
     }
 
     async validateUser(loginOrEmail: string, password: string) {
-        let user: User | null = null;
+        //let user: User | null = null;
 
-        user = await this.usersService.checkCredentials(loginOrEmail, password);
+        //user = await this.usersService.checkCredentials(loginOrEmail, password);
+
+        const user = await this.usersQueryRepository.findOneByLoginOrEmail(loginOrEmail);
 
         if (!user) {
             throw new UnauthorizedException('Invalid credentials');
         }
 
         return user;
-
-        // if (!user) {
-        //     throw new UnauthorizedException('User not found');
-        // }
-        // const isMatch = bcrypt.compare(password, user.password);
-        // if (!isMatch) {
-        //     throw new UnauthorizedException('Password does not match');
-        // }
-        // return user;
     }
 
     async login(user: any) {
@@ -48,25 +47,46 @@ export class AuthService {
         return {accessToken: this.jwtService.sign(payload)};
     }
 
-    async createUser(email, login, password) {
+    async createUser(createUserDto: CreateUserDto) {
 
-        const existsUser = await this.usersQueryRepository.findOneByLoginOrEmail(email);
+        const existsUser = await this.usersQueryRepository.findOneByLoginOrEmail(createUserDto.email);
 
         if (existsUser !== null) {
             throw new BadRequestException('User with this email or login already exists');
         }
 
-        const user = await User.create(login, email, password);
+        const confirmationCode = uuidv4();
+        const saltRounds = 10;
+        const passwordHashed = await  bcrypt.hash(createUserDto.password, saltRounds);
 
-        const createdUser: User = await this.usersRepository.create(user);
+        const newUser: UserDBType ={
+            accountData: {
+                login: createUserDto.login,
+                email: createUserDto.email,
+                passwordHash: passwordHashed,
+                passwordRecoveryCode: "",
+                recoveryCodeExpirationDate: null,
+                createdAt: new Date().toISOString()
+            },
+            emailConfirmation: {
+                confirmationCode: confirmationCode,
+                expirationDate: dateFns.add(new Date(), {
+                    hours: 1,
+                    minutes: 3
+                }),
+                isConfirmed: false
+            }
+        }
 
-        if (!createdUser) {
+        const res = await this.usersRepository.createUser(newUser);
+
+        if (!newUser) {
             throw new BadRequestException('User creation failed');
         }
 
-        await this.emailService.sendEmailConfirmationMessage(user);
+        await this.emailService.sendEmailConfirmationMessage(newUser);
 
-        return user;
+        return newUser;
 
     }
 
