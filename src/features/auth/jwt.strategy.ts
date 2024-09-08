a820696e89dadbf137e8ac869b5467007aeace42
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-08 20:55:52 -0300

    Refactor code: return object

--- src/features/auth/jwt.strategy.ts
+++ src/features/auth/jwt.strategy.ts
@@ -2,8 +2,7 @@ import {Injectable, UnauthorizedException} from "@nestjs/common";
 import {PassportStrategy} from "@nestjs/passport";
 import {Strategy} from "passport-local";
 import {jwtConstants} from "./constants";
-import {ExtractJwt } from 'passport-jwt';
-
+import {ExtractJwt} from 'passport-jwt';
 
 
 @Injectable()
@@ -17,6 +16,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(payload: any) {
-        return payload.sub;
+        return {userId: payload.sub, username: payload.username};
     }
 }
