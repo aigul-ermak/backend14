e3cf05f2200e7261d70c67714cd71be833bb69d3
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-09 10:43:42 -0300

    Added class

--- src/features/users/api/models/output/user.output.model.ts
+++ src/features/users/api/models/output/user.output.model.ts
@@ -1,4 +1,5 @@
-import {UserDocument} from "../../../domain/users.entity";
+import {User, UserDocument} from "../../../domain/users.entity";
+import {OutputUserItemType, UserDBType} from "../../../types/user.types";
 
 
 export class UserOutputModel {
@@ -20,3 +21,33 @@ export const UserOutputModelMapper = (user: UserDocument): UserOutputModel => {
 
     return outputModel;
 }
+
+export const UsersOutputModelMapper = (user: any) => {
+    const outputModel = new UserOutputModel();
+
+    outputModel.id = user.id;
+    outputModel.login = user.accountData.login;
+    outputModel.email = user.accountData.email;
+    outputModel.createdAt = user.accountData.createdAt;
+
+    return outputModel;
+}
+
+// export const userMapper = (user: UserDBType): OutputUserItemType => {
+//     return {
+//         id: user.id.toString(),
+//         accountData: {
+//             login: user.accountData.login,
+//             email: user.accountData.email,
+//             passwordHash: user.accountData.passwordHash,
+//             passwordRecoveryCode: user.accountData.passwordRecoveryCode,
+//             recoveryCodeExpirationDate: user.accountData.recoveryCodeExpirationDate,
+//             createdAt: user.accountData.createdAt
+//         },
+//         emailConfirmation: {
+//             confirmationCode: user.emailConfirmation.confirmationCode,
+//             expirationDate: user.emailConfirmation.expirationDate,
+//             isConfirmed: user.emailConfirmation.isConfirmed
+//         }
+//     }
+// }
