2f7c1ae7a3a30a962cbcf7c777a3f754b3d8b1d2
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-08 21:07:48 -0300

    Refactor getAllUsers end-point

--- src/features/users/api/users.controller.ts
+++ src/features/users/api/users.controller.ts
@@ -15,16 +15,17 @@ import {UserOutputModel} from "./models/output/user.output.model";
 import {UsersQueryRepository} from "../infrastructure/users.query-repository";
 import {BasicAuthGuard} from "../../auth/basic-auth.guard";
 import {GetAllUsersQueryDto} from "./models/output/users.output.model";
+import {SortUserDto} from "./models/output/sort.user.dto";
 
 @Controller('users')
 export class UsersController {
-    userService: UsersService;
+    usersService: UsersService;
 
     constructor(
         userService: UsersService,
         private readonly usersQueryRepository: UsersQueryRepository,
     ) {
-        this.userService = userService;
+        this.usersService = userService;
     }
 
     @UseGuards(BasicAuthGuard)
@@ -34,13 +35,13 @@ export class UsersController {
         @Body() createUserDto: CreateUserDto,
     ): Promise<UserOutputModel> {
 
-        const userId = await this.userService.createUser(
+        const userId = await this.usersService.createUser(
             createUserDto.email,
             createUserDto.login,
             createUserDto.password,
         );
 
-        const user = await this.usersQueryRepository.getById(userId);
+        const user = await this.usersQueryRepository.getUserById(userId);
 
         if (!user) {
             throw new NotFoundException('User not found');
@@ -50,40 +51,45 @@ export class UsersController {
     }
 
     @Get()
-    async getAllUsers(
-        @Query() query: GetAllUsersQueryDto) {
-        const sort = query.sortBy ?? 'accountData.createdAt';
-        const direction = query.sortDirection?.toLowerCase() === 'asc' ? 'asc' : 'desc';
-        const page = query.pageNumber ?? 1;
-        const size = query.pageSize ?? 10;
-        const searchLogin = query.searchLoginTerm ?? '';
-        const searchEmail = query.searchEmailTerm ?? '';
-
-        //return this.userService.findAll();
-        const {users, totalCount} = await this.userService.findAllPaginated(
-            sort,
-            direction,
-            page,
-            size,
-            searchLogin,
-            searchEmail,
-        );
-        const pagesCount = Math.ceil(totalCount / size);
-
-        return {
-            pagesCount,
-            page: +page,
-            pageSize: +size,
-            totalCount,
-            items: users,
-        };
+    async getAllUsers(@Query() sortData: SortUserDto) {
+        return this.usersService.getAllUsers(sortData);
     }
+
+    // @Get()
+    // async getAllUsers(
+    //     @Query() query: GetAllUsersQueryDto) {
+    //     const sort = query.sortBy ?? 'accountData.createdAt';
+    //     const direction = query.sortDirection?.toLowerCase() === 'asc' ? 'asc' : 'desc';
+    //     const page = query.pageNumber ?? 1;
+    //     const size = query.pageSize ?? 10;
+    //     const searchLogin = query.searchLoginTerm ?? '';
+    //     const searchEmail = query.searchEmailTerm ?? '';
+    //
+    //     //return this.userService.findAll();
+    //     const {users, totalCount} = await this.userService.getAllUsers(
+    //         sort,
+    //         direction,
+    //         page,
+    //         size,
+    //         searchLogin,
+    //         searchEmail,
+    //     );
+    //     const pagesCount = Math.ceil(totalCount / size);
+    //
+    //     return {
+    //         pagesCount,
+    //         page: +page,
+    //         pageSize: +size,
+    //         totalCount,
+    //         items: users,
+    //     };
+    // }
 
     @UseGuards(BasicAuthGuard)
     @Delete(':id')
     @HttpCode(204)
     async deleteUser(@Param('id') id: string): Promise<void> {
-        const result = await this.userService.deleteUserById(id);
+        const result = await this.usersService.deleteUserById(id);
         if (!result) {
             throw new NotFoundException(`User with id ${id} not found`);
         }
