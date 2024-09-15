a2ddf9aa8ebea6511e3b5b24015bbda3283a202c
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-15 13:50:35 -0300

    Modified blog query repo: removed mapper from getBlogById

--- src/features/blogs/infrastructure/blogs.query-repository.ts
+++ src/features/blogs/infrastructure/blogs.query-repository.ts
@@ -2,7 +2,6 @@ import {Injectable} from "@nestjs/common";
 import {InjectModel} from "@nestjs/mongoose";
 import {Blog, BlogDocument} from "../domain/blog.entity";
 import {Model} from "mongoose";
-import {BlogOutputModel, BlogOutputModelMapper} from "../api/models/output/blog.output.model";
 
 
 @Injectable()
@@ -10,14 +9,7 @@ export class BlogsQueryRepository {
     constructor(@InjectModel(Blog.name) private blogModel: Model<BlogDocument>) {
     }
 
-    async getBlogById(blogId: string): Promise<BlogOutputModel | null> {
-        const blog = this.blogModel.findById(blogId).exec();
-
-        if (!blog) {
-            return null
-        }
-
-        return BlogOutputModelMapper(blog);
-
+    async getBlogById(blogId: string) {
+        return await this.blogModel.findById(blogId).exec();
     }
 }
